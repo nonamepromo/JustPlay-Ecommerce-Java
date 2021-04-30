999f1490ca3151a56609876cd84e3e7be21c28e9
nonamepromo <scaruso96@gmail.com>
2021-04-30 23:06:36 +0200

    Angelo

--- justplay/src/main/java/it/univaq/disim/mwt/justplay/JustPlayApplication.java
+++ justplay/src/main/java/it/univaq/disim/mwt/justplay/JustPlayApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import it.univaq.disim.mwt.justplay.presentation.VideogiocoController;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = VideogiocoController.class)
 public class JustPlayApplication {
 
 	public static void main(String[] args) {
