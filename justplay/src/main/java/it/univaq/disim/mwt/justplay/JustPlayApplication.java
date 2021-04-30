0127c12c5afc56a2df17299b8ec78768ad250eb1
nonamepromo <scaruso96@gmail.com>
2021-04-30 21:57:49 +0200

    Risolto problema white page

--- justplay/src/main/java/it/univaq/disim/mwt/justplay/JustPlayApplication.java
+++ justplay/src/main/java/it/univaq/disim/mwt/justplay/JustPlayApplication.java
@@ -2,8 +2,12 @@ package it.univaq.disim.mwt.justplay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import it.univaq.disim.mwt.justplay.presentation.VideogiocoController;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = VideogiocoController.class)
 public class JustPlayApplication {
 
 	public static void main(String[] args) {
