83f9a6dea6aaad1ebda633f657da63a2595f3e27
nonamepromo <scaruso96@gmail.com>
2021-05-24 17:46:56 +0200

    Login + javascript + dati giochi in details

---
+++ justplay-1/src/main/java/it/univaq/disim/mwt/justplay/business/impl/jdbc/JDBCUtenteServiceImpl.java
@@ -0,0 +1,80 @@
+package it.univaq.disim.mwt.justplay.business.impl.jdbc;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.univaq.disim.mwt.justplay.business.BusinessException;
+import it.univaq.disim.mwt.justplay.business.UtenteService;
+import it.univaq.disim.mwt.justplay.domain.Utente;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Transactional
+@Slf4j
+public class JDBCUtenteServiceImpl implements UtenteService {
+	
+	private static final String FIND_USERNAME = "SELECT * FROM utenti u WHERE u.username = ?";
+	private static final String UPDATE_PROFILE = "UPDATE utenti SET email=?, codice_fiscale=? , data_nascita=?, telefono=? WHERE id_utente=?";
+	
+	@Autowired
+	private DataSource dataSource;
+
+	@Override
+	public Utente findUtenteByUsername(String username) throws BusinessException {
+		Utente utente = null;
+		try (Connection con = dataSource.getConnection(); PreparedStatement st = con.prepareStatement(FIND_USERNAME);) {
+			st.setString(1, username);
+			try (ResultSet rs = st.executeQuery();) {
+				if (rs.next()) {
+					int tipologia = rs.getInt("tipologia_utente");
+					utente = new Utente();
+					utente.setId(rs.getLong("id_utente"));
+					utente.setUsername(username);
+					utente.setPassword(rs.getString("password"));
+					utente.setCognome(rs.getString("cognome"));
+					utente.setNome(rs.getString("nome"));
+					utente.setEmail(rs.getString("email"));
+					utente.setDataNascita(rs.getObject("data_nascita", LocalDate.class));
+				} else {
+					log.error("errore nella find dell'utente " + username);
+					throw new BusinessException();
+				}
+
+			}
+		} catch (SQLException e) {
+			log.error("findUtenteByUsername", e);
+			throw new BusinessException("findUtenteByUsername", e);
+		}
+		return utente;
+	}
+
+	@Override
+	public void updateProfilo(Utente nuovoProfilo) throws BusinessException {
+
+		try (Connection con = dataSource.getConnection();
+				PreparedStatement st = con.prepareStatement(UPDATE_PROFILE);) {
+			st.setString(1, nuovoProfilo.getEmail());
+			st.setDate(3, Date.valueOf(nuovoProfilo.getDataNascita()));
+			st.setLong(5, nuovoProfilo.getId());
+			st.executeUpdate();
+
+		} catch (SQLException e) {
+			log.error("updateProfilo", e);
+			throw new BusinessException("updateProfilo", e);
+		}
+
+	}
+
+}
---
+++ justplay-1/src/main/java/it/univaq/disim/mwt/justplay/security/UserDetailsImpl.java
@@ -0,0 +1,72 @@
+package it.univaq.disim.mwt.justplay.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import it.univaq.disim.mwt.justplay.domain.Utente;
+
+public class UserDetailsImpl implements UserDetails {
+	
+	private static final String ROLE_PREFIX = "ROLE_";
+	
+	private Utente utente;
+
+	public UserDetailsImpl(Utente utente) {
+		super();
+		this.utente = utente;
+	}
+
+
+	@Override
+	public String getPassword() {
+		return utente.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return utente.getUsername();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "UserDetailsImpl [username=" + utente.getUsername() + "]";
+	}
+
+	public Utente getUtente() {
+		return utente;
+	}
+
+
+	@Override
+	public Collection<GrantedAuthority> getAuthorities() {
+		return null;
+	}
+	
+
+	
+}
