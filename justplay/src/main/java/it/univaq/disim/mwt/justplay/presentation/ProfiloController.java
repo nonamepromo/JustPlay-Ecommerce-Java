fce86d7a8b14b05ade832aba7f255480e6e39a23
nonamepromo <scaruso96@gmail.com>
2021-07-12 21:43:10 +0200

    Fix Profilo utente

--- justplay/src/main/java/it/univaq/disim/mwt/justplay/presentation/ProfiloController.java
+++ justplay/src/main/java/it/univaq/disim/mwt/justplay/presentation/ProfiloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import it.univaq.disim.mwt.justplay.business.BusinessException;
@@ -20,7 +21,6 @@ public class ProfiloController {
 
 	@Autowired
 	private UtenteService service;
-	private VideogiocoService gameService;
 
 	@GetMapping
 	public String modificaProfiloStart(Model model) throws BusinessException {
