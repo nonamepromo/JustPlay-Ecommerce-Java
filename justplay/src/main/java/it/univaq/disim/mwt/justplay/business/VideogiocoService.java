5878e6ccca68842674349a197df2ec7bb928ac6d
nonamepromo <scaruso96@gmail.com>
2021-11-09 22:40:51 +0100

    Add Videogioco Service

--- justplay/src/main/java/it/univaq/disim/mwt/justplay/business/VideogiocoService.java
+++ justplay/src/main/java/it/univaq/disim/mwt/justplay/business/VideogiocoService.java
@@ -1,6 +1,22 @@
 package it.univaq.disim.mwt.justplay.business;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import it.univaq.disim.mwt.justplay.domain.Utente;
+import it.univaq.disim.mwt.justplay.domain.Videogioco;
+import it.univaq.disim.mwt.justplay.domain.VideogiocoInVendita;
+
 public interface VideogiocoService{
+	
+	Videogioco findById(Long id) throws BusinessException;
+	
+	Page<Videogioco> findAll(Pageable pageable) throws BusinessException;
+		
+	void addGameToSellinglist(VideogiocoInVendita videogiocoInVendita, Videogioco videogioco, Utente  utente) throws BusinessException;
 
+	void removeGameFromSellinglist(Videogioco videogioco, Utente utente, VideogiocoInVendita videogiocoInVendita) throws BusinessException;
 
+	void removeGameFromLikedlist(Utente utente, Videogioco videogioco) throws BusinessException;
+	
 }
