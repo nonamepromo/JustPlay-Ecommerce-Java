b0f28c7325ae77d2c2d10dc3ce720a3d7c8c7280
nonamepromo <scaruso96@gmail.com>
2021-11-13 18:33:20 +0100

    fix

--- justplay/src/main/java/it/univaq/disim/mwt/justplay/business/impl/mongodb/CommentoServiceImpl.java
+++ justplay/src/main/java/it/univaq/disim/mwt/justplay/business/impl/mongodb/CommentoServiceImpl.java
@@ -1,5 +1,41 @@
 package it.univaq.disim.mwt.justplay.business.impl.mongodb;
 
-public class CommentoServiceImpl {
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import it.univaq.disim.mwt.justplay.business.BusinessException;
+import it.univaq.disim.mwt.justplay.business.CommentoService;
+import it.univaq.disim.mwt.justplay.domain.Commento;
+import it.univaq.disim.mwt.justplay.domain.Utente;
+import it.univaq.disim.mwt.justplay.domain.Videogioco;
+
+@Service
+@Transactional
+public class CommentoServiceImpl implements CommentoService{
+
+	@Override
+	public Commento findById(Long id) throws BusinessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addCommento(Videogioco videogioco, Utente utente, String contenuto) throws BusinessException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateCommento(Commento commento) throws BusinessException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteCommento(Commento commento) throws BusinessException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
