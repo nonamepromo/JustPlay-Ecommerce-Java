633894657033447bd8634ea40003a5970cffdaa7
nonamepromo <scaruso96@gmail.com>
2021-11-21 11:39:36 +0100

    fix

--- justplay/src/main/java/it/univaq/disim/mwt/justplay/business/ConversazioneService.java
+++ justplay/src/main/java/it/univaq/disim/mwt/justplay/business/ConversazioneService.java
@@ -8,21 +8,11 @@ import it.univaq.disim.mwt.justplay.domain.Conversazione;
 import it.univaq.disim.mwt.justplay.domain.Messaggio;
 
 public interface ConversazioneService {
-	
-	//Conversazione findById(String id) throws BusinessException;
 
 	Conversazione findConversazioneByUsernames(Set<String> partecipanti) throws BusinessException;
-	
-	//Conversazione inserisci(Conversazione conversazione) throws BusinessException;
-	
-	//List<Conversazione> findAll() throws BusinessException;
 
 	List<Conversazione> findAllByUsername(String username) throws BusinessException;
 	
 	void addOrUpdateConversazione(Conversazione conversazione) throws BusinessException;
-	
-	//Optional<Conversazione> findByPartecipanti(List<String> partecipanti) throws BusinessException;
-	
-	//void invia(Messaggio messaggio) throws BusinessException;
 	
 }
